5b05226c0c07d0e519a3e60f7dd8fbfd3b4f7b01
Dmitrii Kuzmenkov <kuzmenkov.job@gmail.com>
2022-11-03 23:20:09 +0300

    SQL database work started

---
+++ TrackerLibrary/TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+        public static void InitialiseDataConnections(bool sqlStorage, bool txtStorage)
+        { 
+            if (sqlStorage)
+            {
+                //TODO - create SQL connection
+            }
+
+            if (txtStorage)
+            {
+                //TODO - create txt connection
+            }
+        }
+    }
+}
